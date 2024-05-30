ca2f751574da842ff8361ac3352d47c199a8a8f2
Tanush Mahajan <tanush.mohina@gmail.com>
2024-05-30 14:22:11 +0530

    feat: Added LinkedIn HR template in page.tsx
    This commit adds a new LinkedIn template to the `page.tsx` file. The template includes a personalized message for LinkedIn recruiters, highlighting the applicant's passion for web development, versatility, and experience with various technologies. It also provides contact information and a link to the applicant's resume. This addition enhances the content available for LinkedIn recruiters and improves the chances of getting noticed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,33 @@ export default function Home() {
 			variables: ["userName", "jobLink", "companyName"],
 			// resumeAttachedFlag
 		},
+		{
+			templateText: `
+      Hi $.userName,
+
+      It's great connecting with you. Hope you're doing well.
+
+      I know you must get an insane volume of applicants so I wanted to take this opportunity to focus on 5 Bullet points that would help you get a better idea of me as a person and why I want to join $.companyName's journey.
+
+      - I'm deeply passionate about the dynamic world of Web, JavaScript and the ever-evolving React and Web Ecosystem, constantly immersing myself in the latest technologies. I've had the privilege of spearheading frontend migrations to Next.js, React (Typescript), and TailwindCSS, crafting seamless user experiences for more than 2-3 years now in my current role at Optum.
+
+      - At Optum, I've been thrown into multiple hats, ranging from Web-Development (Frontend/Backend), AI/ML projects, NLP etc. This flexibility mirrors the startup ethos, where versatility is key.
+
+      - With each project, I take ownership as if it were my own, pouring my energy into delivering exceptional & creative results that push boundaries.
+
+      - I'm accustomed to working in fast paced environments with high developer velocity.
+
+      Thank you for considering my application. I am looking forward to the possibility of joining $.companyName's journey. I am available for an interview at your earliest convenience and can be reached at +91-7988187982 or via email at taush.mohina@gmail.com.
+
+      Here is a link to my resume:
+      #.resumeLink
+
+      Best Regards 
+      Tanush Mahajan
+      `,
+			title: "Linked IN Recruiters",
+			variables: ["userName", "companyName", "jobLink"],
+		},
 	];
 	// const resumeLinkFromStorage = "";
 	const [resumeInput, setResumeInput] = useState<string>("");
