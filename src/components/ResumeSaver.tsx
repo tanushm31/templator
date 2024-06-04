ae68aab000c496e40ab3d980e2d3a3771c46cad7
Tanush Mahajan <tanush.mohina@gmail.com>
2024-06-04 22:16:11 +0530

    Profile Section For Custom User

--- src/components/ResumeSaver.tsx
+++ src/components/ResumeSaver.tsx
@@ -1,9 +1,98 @@
 "use client";
+
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 type IResumeSaveSectionProps = {
 	resumeLink: string;
 	setResumeLink: (resumeLink: string) => void;
 	resumeInput: string;
 	setResumeInput: (resumeInput: string) => void;
+};
+
+export type IProfileFormData = {
+	name: string;
+	email: string;
+	phone: string;
+	resumeLink: string;
+};
+export type IProfileSignUpProps = {
+	name: string;
+	email: string;
+	phone: string;
+	resumeLink: string;
+	saveProfile: (data: IProfileFormData) => void;
+};
+export const ProfileSignUp = (props: IProfileSignUpProps) => {
+	const [name, setName] = useState(props.name);
+	const [email, setEmail] = useState(props.email);
+	const [phone, setPhone] = useState(props.phone);
+	const [resumeLink, setResumeLink] = useState(props.resumeLink);
+
+	return (
+		<div className="flex flex-col w-full p-5 my-2 border-2 border-gray-300 rounded">
+			{/* Form To Enter or Update Profile Section */}
+			<div>Add Profile Details:</div>
+			<div className="flex flex-col space-y-2">
+				<form className="grid grid-cols-1 gap-2 md:grid-cols-2">
+					<div className="grid items-center justify-start grid-cols-3 p-2 mt-1 space-x-2 bg-gray-700 rounded ">
+						<label>Name:</label>
+						<input
+							type="text"
+							defaultValue={props.name}
+							onChange={(e) => setName(e.target.value)}
+							placeholder="Name"
+							className="col-span-2 px-3 py-2 text-black rounded"
+						/>
+					</div>
+					<div className="grid items-center justify-start grid-cols-3 p-2 mt-1 space-x-2 bg-gray-700 rounded ">
+						<label>Email:</label>
+						<input
+							type="email"
+							defaultValue={props.email}
+							onChange={(e) => setEmail(e.target.value)}
+							placeholder="Email"
+							className="col-span-2 px-3 py-2 text-black rounded"
+						/>
+					</div>
+					<div className="grid items-center justify-start grid-cols-3 p-2 mt-1 space-x-2 bg-gray-700 rounded ">
+						<label>Phone:</label>
+						<input
+							type="tel"
+							defaultValue={props.phone}
+							onChange={(e) => setPhone(e.target.value)}
+							placeholder="Phone"
+							className="col-span-2 px-3 py-2 text-black rounded"
+						/>
+					</div>
+					<div className="grid items-center justify-start grid-cols-3 p-2 mt-1 space-x-2 bg-gray-700 rounded ">
+						<label>Resume Link:</label>
+						<input
+							type="url"
+							defaultValue={props.resumeLink}
+							onChange={(e) => setResumeLink(e.target.value)}
+							placeholder="Resume Link"
+							className="col-span-2 px-3 py-2 text-black rounded"
+						/>
+					</div>
+				</form>
+				<button
+					onClick={() => {
+						props.saveProfile({
+							name,
+							email,
+							phone,
+							resumeLink,
+						});
+						toast("Profile Details Saved Successfully");
+					}}
+					className="flex items-center justify-center px-3 py-2 bg-green-700 rounded"
+				>
+					Save
+				</button>
+			</div>
+		</div>
+	);
 };
 const ResumeSaveSection = ({
 	resumeLink,
@@ -12,9 +101,9 @@ const ResumeSaveSection = ({
 	setResumeInput,
 }: IResumeSaveSectionProps) => {
 	return (
-		<div className="p-5 border-gray-300 border-2 rounded flex w-full flex-col my-2">
-			<div className="flex w-full justify-start space-x-5 items-center text-gray-300">
-				<span className="flex justify-center items-center">
+		<div className="flex flex-col w-full p-5 my-2 border-2 border-gray-300 rounded">
+			<div className="flex items-center justify-start w-full space-x-5 text-gray-300">
+				<span className="flex items-center justify-center">
 					{resumeLink ? "Current Link" : "No Link Found"}
 				</span>
 				{/* <div className="flex justify-start items-center space-x-2 w-[80%]">
@@ -22,14 +111,14 @@ const ResumeSaveSection = ({
 				{resumeLink && (
 					<a
 						href={resumeLink}
-						className="px-3 py-2 rounded text-blue-300 truncate"
+						className="px-3 py-2 text-blue-300 truncate rounded"
 					>
 						{resumeLink}
 					</a>
 				)}
-				{/* <button className="flex px-3 py-2 rounded bg-green-700">Save</button> */}
+				{/* <button className="flex px-3 py-2 bg-green-700 rounded">Save</button> */}
 			</div>
-			<div className="flex w-full space-x-5 justify-start items-center text-gray-300">
+			<div className="flex items-center justify-start w-full space-x-5 text-gray-300">
 				<span>Resume Link</span>
 				<input
 					onChange={(e) => {
@@ -37,13 +126,13 @@ const ResumeSaveSection = ({
 					}}
 					className="px-3 py-2 rounded text-black w-[70vw]"
 				/>
-				{/* <div className="flex justify-start items-center space-x-2">
+				{/* <div className="grid items-center justify-start grid-cols-2 p-2 mt-1 space-x-2 bg-gray-700 rounded ">
 					</div> */}
 				<button
 					onClick={() => {
 						setResumeLink(resumeInput);
 					}}
-					className="flex px-3 py-2 rounded bg-green-700"
+					className="flex px-3 py-2 bg-green-700 rounded"
 				>
 					Save
 				</button>
